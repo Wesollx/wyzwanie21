32d6f28f2e26217019d554f42417881f35423b61
Wesolxxx <j.wesolekk@gmail.com>
2024-04-19 11:25:08 +0200

    homework dzien 4

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,16 @@
-﻿internal class Program
+﻿var name = "Piotr";
+var sex = 'm';
+var age = 17;
+
+if (sex == 'k' && age < 30)
 {
-    private static void Main(string[] args)
-    {
-        Console.WriteLine($"{args[0]}");
-    }
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (sex == 'm' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni męzczyzna");
 }

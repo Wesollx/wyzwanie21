2f35dfb10950e83214600a718416c5bb88162f73
Wesolxxx <j.wesolekk@gmail.com>
2024-04-24 15:02:33 +0200

    tests

---
+++ src/wyzwanie21Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace wyzwanie21Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}

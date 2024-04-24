67d3eaa4fe9dbab7b81fd2c375a137f54f79278a
Wesolxxx <j.wesolekk@gmail.com>
2024-04-24 15:13:46 +0200

    homework dzien6 pop

--- src/Program.cs
+++ src/Program.cs
@@ -1,40 +1,40 @@
 ﻿
-Employee e1 = new Employee("Anna","Kibol","32");
-Employee e2 = new Employee("Jan","Zaraza","20");
-Employee e3 = new Employee("Ola","Molenda","56");
+Employee employee1 = new Employee("Anna","Kibol","32");
+Employee employee2 = new Employee("Jan","Zaraza","20");
+Employee employee3 = new Employee("Ola","Molenda","56");
 
-e1.AddGrade(10);
-e1.AddGrade(10);
-e1.AddGrade(8);
-e1.AddGrade(10);
-e1.AddGrade(2);
+employee1.AddGrade(10);
+employee1.AddGrade(10);
+employee1.AddGrade(8);
+employee1.AddGrade(10);
+employee1.AddGrade(2);
 
-e2.AddGrade(3);
-e2.AddGrade(10);
-e2.AddGrade(9);
-e2.AddGrade(7);
-e2.AddGrade(2);
+employee2.AddGrade(3);
+employee2.AddGrade(10);
+employee2.AddGrade(9);
+employee2.AddGrade(7);
+employee2.AddGrade(2);
 
-e3.AddGrade(1);
-e3.AddGrade(1);
-e3.AddGrade(3);
-e3.AddGrade(5);
-e3.AddGrade(10);
+employee3.AddGrade(1);
+employee3.AddGrade(1);
+employee3.AddGrade(3);
+employee3.AddGrade(5);
+employee3.AddGrade(10);
 
 List<Employee> employees = new List<Employee>()
 {
-    e1, e2, e3
+    employee1, employee2, employee3
 };
 
 var MaxScore = -1;
 Employee BestEmployee = null;
 
-foreach (var e in employees)
+foreach (var employee in employees)
 {
-    if (e.FinalScore() > MaxScore)
+    if (employee.FinalScore() > MaxScore)
     {
-        MaxScore = e.FinalScore();
-        BestEmployee = e;
+        MaxScore = employee.FinalScore();
+        BestEmployee = employee;
     }
 
 }
